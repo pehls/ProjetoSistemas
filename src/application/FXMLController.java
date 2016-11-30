7f7c2a2ba98a220fb7ad068658ebb7d17a132a0d
Gabriel Pehls <gabrielpehls@hotmail.com>
2016-11-30 21:14:12 -0200

    maps ok

--- src/application/FXMLController.java
+++ src/application/FXMLController.java
@@ -16,7 +16,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-
+//
 public class FXMLController implements Initializable, MapComponentInitializedListener {
     
     @FXML
